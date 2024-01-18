a2a8a153eb235d32be4c7e15be77116c4a028c96
Shenhav <shenhavbarziv@gmail.com>
2024-01-18 17:06:08 +0000

    The full server side

---
+++ index.js
@@ -0,0 +1,243 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const app = express();
+const jwt = require("jsonwebtoken");
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// app.use(
+//   cors({
+//     origin: "http://localhost",
+//     credentials: true,
+//   })
+// );
+const dotenv = require('dotenv');
+dotenv.config();
+const {
+  AddRegister,
+  Login,
+  List,
+  ListReg,
+  DeleteReg,
+  ApproveReg,
+  DeleteUser,
+  GetUserById,
+  UpdateUser,
+} = require("./mongo/conn");
+const PORT = 5000;
+app.use(cors({
+  origin: 'http://95.216.153.158',
+  credentials: true,}));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+const disallowedTags = ["script", "iframe", "style"]; // Add more as needed
+function validateInput(user) {
+  for (const [key, value] of Object.entries(user)) {
+    for (const tag of disallowedTags) {
+      if (
+        value === "" ||
+        value.length > 50 ||
+        value.includes(`<${tag}`) ||
+        value.includes(`</${tag}>`)
+      ) {
+        return false;
+      }
+    }
+  }
+  return true;
+}
+app.get("/api", (req,res) => {
+  res.send("hiiiiiii")
+  
+})
+app.post("/api/login", async (req, res) => {
+  const { email, password } = req.body;
+  if (validateInput(req.body)) {
+    const data = await Login(email, password);
+    if (data.user) {
+      const token = createSecretToken(data.user._id);
+      res.cookie("token", token, {
+        httpOnly: false,
+        sameSite: "None",
+        secure: false,
+      });
+      
+      res.cookie("email", req.body.email, {
+        httpOnly: false,
+        sameSite: "None",
+        secure: false,
+      });
+      
+    }
+    res.status(200).json(data);
+  } else {
+    res.json({ msg: "One of the inputs is invalid" });
+  }
+});
+app.post("/api/register", async (req, res) => {
+  user = filterKeys(req.body, [
+    "email",
+    "password",
+    "name",
+    "job",
+    "birthDate",
+    "phoneNumber",
+    "position",
+    "hireDate",
+  ]); //the filter is for unwanted fields that hackers can add in the request(for example: admin:true)
+  if (validateInput(req.body)) {
+    code = await AddRegister(user);
+    if (code === 201) {
+      res.cookie("email", user.email, {
+        withCredentials: true,
+        httpOnly: false,
+        sameSite: "None",
+        secure: true,
+      });
+    }
+    res.json({ code });
+    //201 for successfully created 409 for conflict
+  } else {
+    res.json({ code: 400, msg: "One of the inputs is invalid" });
+  }
+});
+app.get("/api/list", async (req, res) => {
+  const userVerification = req.cookies
+    ? await UserVerification(req.cookies.token)
+    : { status: false };
+  if (userVerification.status) {
+    res.json({
+      status: true,
+      data: await List(),
+      admin: userVerification.user.admin,
+    });
+  } else {
+    res.json({ status: false });
+  }
+});
+function filterKeys(data, allowedKeys) {
+  const filteredData = {};
+  allowedKeys.forEach((key) => {
+    if (data[key] !== undefined) {
+      filteredData[key] = data[key];
+    }
+  });
+  return filteredData;
+}
+app.get("/api/profile", async (req, res) => {
+  const userVerification = req.cookies
+    ? await UserVerification(req.cookies.token)
+    : { status: false };
+  res.json(userVerification);
+});
+app.get("/api/approve", async (req, res) => {
+  const userVerification = req.cookies
+    ? await UserVerification(req.cookies.token)
+    : { status: false };
+  if (userVerification.status && userVerification.user.admin) {
+    res.json({ status: true, data: await ListReg() });
+  } else if (userVerification.status) {
+    res.json({ status: "notAdmin", data: {} });
+  } else {
+    res.json({ status: "notLoggedIn", data: {} });
+  }
+});
+app.delete("/api/approve", async (req, res) => {
+  const userVerification = req.cookies
+    ? await UserVerification(req.cookies.token)
+    : { status: false };
+  if (userVerification.status && userVerification.user.admin) {
+    res.json(await DeleteReg(req.body.id));
+  } else {
+    res.json({
+      status: "Access Denied: You are not authorized to perform this action.",
+    });
+  }
+});
+
+app.post("/api/approve", async (req, res) => {
+  const userVerification = req.cookies
+    ? await UserVerification(req.cookies.token)
+    : { status: false };
+  if (userVerification.status && userVerification.user.admin) {
+    res.json(await ApproveReg(req.body.id));
+  } else {
+    res.json({
+      status: "Access Denied: You are not authorized to perform this action.",
+    });
+  }
+});
+app.delete("/api/del", async (req, res) => {
+  const userVerification = req.cookies
+    ? await UserVerification(req.cookies.token)
+    : { status: false };
+  if (userVerification.status && userVerification.user.admin) {
+    res.json({
+      status: true,
+      data: await DeleteUser(req.body.id),
+      admin: userVerification.user.admin,
+    });
+  } else {
+    res.json({ status: "" });
+  }
+  res.json();
+});
+app.get("/api/EditUser", async (req, res) => {
+  const userVerification = req.cookies
+    ? await UserVerification(req.cookies.token)
+    : { status: false };
+  if (userVerification.status) {
+    const userData = await GetUserById(req.query.id);
+    res.json({
+      status: true,
+      data: userData,
+      admin: userVerification.user.admin,
+    });
+  } else {
+    res.json({ status: "" });
+  }
+});
+app.post("/api/SaveUserChanges", async (req, res) => {
+  data = req.body;
+  const userVerification = req.cookies
+    ? await UserVerification(req.cookies.token)
+    : { status: false };
+  if (userVerification.status && userVerification.user.admin) {
+    res.json({
+      status: true,
+      data: await UpdateUser(data.userId, data.userData),
+      admin: userVerification.user.admin,
+    });
+  } else {
+    res.json({
+      status: "Access Denied: You are not authorized to perform this action.",
+    });
+  }
+});
+function createSecretToken(id) {
+  
+  return jwt.sign({ id }, process.env.TOKEN_KEY, {
+    expiresIn: 24 * 60 * 60,
+  });
+}
+async function UserVerification(token) {
+  if (!token) {
+    return { status: false };
+  }
+  try {
+    const data = await jwt.verify(token, process.env.TOKEN_KEY);
+    const user = await GetUserById(data.id);
+    if (Object.keys(user).length > 0) {
+      return { status: true, user: user };
+    } else {
+      return { status: false };
+    }
+  } catch (err) {
+    return { status: false };
+  }
+}
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
---
+++ mongo/conn.js
@@ -0,0 +1,234 @@
+const { MongoClient, ObjectId, ServerApiVersion  } = require('mongodb');
+require('dotenv').config();
+const { compare, hash } = require('bcryptjs');
+const dbName = "users";
+const registerCollectionName = "register";
+const userCollectionName = "users";
+
+async function Connect() {
+  const client = new MongoClient(process.env.URI, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    }
+  });
+  await client.connect();
+  return client.db(dbName);
+}
+
+
+
+async function AddRegister(user) {
+  try {
+    const db = await Connect();
+    const registerCollection = db.collection(registerCollectionName);
+    const existingUser = await registerCollection.findOne({ email: user.email });
+
+    if (existingUser) {
+      console.log(`User with email ${user.email} already exists in the register collection.`);
+      return 409; // Conflict status code
+    }
+    user.admin = false;
+    const collection = db.collection(registerCollectionName);
+    user.password = await hash(user.password, 10)
+    await collection.insertOne(user);
+    console.log(`${user.name} inserted successfully.\n`);
+    return 201; // Successfully inserted status code
+  } catch (err) {
+    console.error(`Something went wrong trying to insert the new documents: ${err}\n`);
+    return 500; // Internal Server Error status code
+  }
+}
+
+async function Login(email, password) {
+  try {
+    const db = await Connect();
+    let collection = db.collection(userCollectionName);
+    
+    let user = await collection.findOne({ email });
+
+    if (user) {
+      const passwordMatch = await compare(password, user.password);
+
+      if (passwordMatch) {
+        return { user, msg: "Connected successfully" };
+      } else {
+        return { msg: "Email or password are incorrect" };
+      }
+    }
+
+    // No user found in the first collection, checking the second collection
+    collection = db.collection(registerCollectionName);
+    user = await collection.findOne({ email });
+
+    if (user) {
+      const passwordMatch = await compare(password, user.password);
+
+      if (passwordMatch) {
+        return { msg: "Your user is not approved. Please contact your supervisor" };
+      }
+    }
+
+    // No user found in the second collection either
+    return { msg: "Email or password are incorrect" };
+  } catch (err) {
+    console.error("Error during login:", err);
+    return { msg: "An error occurred during login" };
+  }
+}
+
+async function List() {
+  try {
+    const db = await Connect();
+    const collection = db.collection(userCollectionName);
+    const data = await collection.find().toArray();
+    return data;
+  } catch (err) {
+    console.error(`Error finding users: ${err}`);
+    throw err;
+  }
+}
+/*
+async function GetUserByEmail(email) {
+  try {
+    const db = await Connect();
+    const collection = db.collection(userCollectionName);
+    const user = await collection.findOne({ email });
+    const keysToKeep = ['email', 'name', 'job', 'birthDate', 'phoneNumber', 'position', 'hireDate'];
+    const ans = Object.fromEntries(
+      Object.entries(user).filter(([key]) => keysToKeep.includes(key))
+    );
+    return ans;
+  } catch (err) {
+    console.error(`Error finding user by email: ${err}`);
+    throw err;
+  }
+}
+*/
+async function ListReg() {
+  try {
+    const db = await Connect();
+    const collection = db.collection(registerCollectionName);
+    const data = await collection.find().toArray();
+    if(!data){return {};}
+    return data;
+  } catch (err) {
+    console.error(`Error finding users: ${err}`);
+    throw err;
+  }
+}
+async function DeleteReg(id) {
+  try {
+    const db = await Connect();
+    const collection = db.collection(registerCollectionName);
+    const result = await collection.deleteOne({ _id: new ObjectId(id) });
+
+    if (result.deletedCount === 1) {
+      console.log(`User with id ${id} deleted successfully from the register collection.`);
+      return {status:200}; // Success status code
+    } else {
+      return {status:404}; // Not Found status code
+    }
+  } catch (err) {
+    console.error(`Error deleting user from register collection: ${err}`);
+    return {status:500};
+  }
+}
+
+async function ApproveReg(id) {
+  try {
+    const db = await Connect();
+    const registerCollection = db.collection(registerCollectionName);
+    const userCollection = db.collection(userCollectionName);
+
+    // Find the user to be approved in the register collection
+    const userToApprove = await registerCollection.findOne({ _id: new ObjectId(id) });
+
+    if (userToApprove) {
+      // Insert the user into the user collection
+      await userCollection.insertOne(userToApprove);
+      // Delete the user from the register collection
+      await registerCollection.deleteOne({ _id: new ObjectId(id) });
+
+      console.log(`User with id ${id} approved and moved to the users collection.`);
+      return { status: 200 }; // Success status code
+    } else {
+      console.log(`User with id ${id} not found in the register collection.`);
+      return { status: 404 }; // Not Found status code
+    }
+  } catch (err) {
+    console.error(`Error approving user in register collection: ${err}`);
+    return { status: 500 }; // Internal Server Error status code
+  }
+}
+async function DeleteUser(id) {
+  try {
+    const db = await Connect();
+    const collection = db.collection(userCollectionName);
+    const result = await collection.deleteOne({ _id: new ObjectId(id) });
+
+    if (result.deletedCount === 1) {
+      console.log(`User with id ${id} deleted successfully from the user collection.`);
+      return {status:200};
+    } else {
+      console.log(`User with id ${id} not found in the user collection.`);
+      return {status:404};
+    }
+  } catch (err) {
+    console.error(`Error deleting user from user collection: ${err}`);
+    return {status:500};
+  }
+}
+async function GetUserById(userId)
+{
+  try {
+    console.log(userId);
+    const db = await Connect();
+    const collection = db.collection(userCollectionName);
+    const user = await collection.findOne({ _id: new ObjectId(userId) });
+    
+    if (user) {
+      const keysToKeep = ['email', 'name', 'job', 'birthDate', 'phoneNumber', 'position', 'hireDate', 'admin'];
+    const ans = Object.fromEntries(
+      Object.entries(user).filter(([key]) => keysToKeep.includes(key))
+    );
+      console.log('Found user:', ans);
+      return ans;
+    } else {
+      console.log('User not found');
+      return {};
+    }
+  } catch (err) {
+    console.error(`Error finding user by ID: ${err}`);
+  }
+}
+async function UpdateUser(userId, userData) {
+  try {
+    client = new MongoClient(process.env.URI);
+    await client.connect();
+    
+    const db = client.db(dbName);
+    const collection = db.collection(userCollectionName);
+
+    const result = await collection.updateOne(
+      { _id: new ObjectId(userId) },
+      { $set: userData }
+    );
+
+    if (result.modifiedCount === 1) {
+      console.log(`User with ID ${userId} updated successfully.`);
+      return { code: 200, msg: 'User updated successfully' };
+    } else {
+      console.log(`User with ID ${userId} not found.`);
+      return { code: 404, msg: 'User not found' };
+    }
+  } catch (error) {
+    console.error(`Error updating user: ${error}`);
+    return { code: 500, msg: 'Error' };
+  } finally {
+    await client.close();
+  }
+}
+
+module.exports = { AddRegister, Login, List, ListReg, DeleteReg, ApproveReg, DeleteUser, GetUserById, UpdateUser };
